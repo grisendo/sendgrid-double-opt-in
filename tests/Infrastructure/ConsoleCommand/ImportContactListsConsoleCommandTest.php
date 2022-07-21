76ed59fea6e6def7c80d13b3907d16d27c15f979
Francisco José Cruz Romanos <grisendo@gmail.com>
2022-07-21 13:28:24 +0200

    Console command tests with mocks (#9)

--- tests/Infrastructure/ConsoleCommand/ImportContactListsConsoleCommandTest.php
+++ tests/Infrastructure/ConsoleCommand/ImportContactListsConsoleCommandTest.php
@@ -2,42 +2,133 @@
 
 namespace App\Tests\Infrastructure\ConsoleCommand;
 
+use App\Domain\ContactList\ContactListCreatedDomainEvent;
+use App\Domain\ContactList\ContactListRenamedDomainEvent;
 use App\Infrastructure\Bus\SymfonyMessengerCommandBus;
-use App\Infrastructure\Bus\SymfonyMessengerEventBus;
 use App\Infrastructure\ConsoleCommand\ImportContactListsConsoleCommand;
 use App\Tests\BaseKernelTestCase;
+use App\Tests\Domain\ContactList\ContactListIdMother;
+use App\Tests\Domain\ContactList\ContactListNameMother;
+use Grisendo\DDD\Bus\Event\EventBus;
+use Mockery;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class ImportContactListsConsoleCommandTest extends BaseKernelTestCase
 {
-    private CommandTester $commandTester;
-
-    protected function setUp(): void
+    public function testNoChanges(): void
     {
-        parent::setUp();
         $application = new Application();
+        $eventBus = Mockery::mock(EventBus::class);
+        $eventBus
+            ->shouldReceive('getPublishedEvents')
+            ->once()
+            ->with()
+            ->andReturn([]);
         $application->add(
             new ImportContactListsConsoleCommand(
                 new SymfonyMessengerCommandBus(
                     self::getContainer()->get('command.bus')
                 ),
-                new SymfonyMessengerEventBus(
-                    self::getContainer()->get('event.bus')
-                ),
+                $eventBus
             )
         );
         $command = $application->find(
             ImportContactListsConsoleCommand::getDefaultName()
         );
-        $this->commandTester = new CommandTester($command);
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([]);
+
+        $this->assertEquals(Command::SUCCESS, $commandTester->getStatusCode());
+        $this->assertStringContainsString(
+            '0 list(s) created',
+            $commandTester->getDisplay()
+        );
+        $this->assertStringContainsString(
+            '0 list(s) renamed',
+            $commandTester->getDisplay()
+        );
     }
 
-    public function testFoo(): void
+    public function testListCreated(): void
     {
-        $exitCode = $this->commandTester->execute([]);
+        $application = new Application();
+        $eventBus = Mockery::mock(EventBus::class);
+        $eventBus
+            ->shouldReceive('getPublishedEvents')
+            ->once()
+            ->with()
+            ->andReturn([
+                new ContactListCreatedDomainEvent(
+                    ContactListIdMother::random()->getValue()->getValue(),
+                    ContactListNameMother::random()->getValue()
+                ),
+            ]);
+        $application->add(
+            new ImportContactListsConsoleCommand(
+                new SymfonyMessengerCommandBus(
+                    self::getContainer()->get('command.bus')
+                ),
+                $eventBus
+            )
+        );
+        $command = $application->find(
+            ImportContactListsConsoleCommand::getDefaultName()
+        );
+        $commandTester = new CommandTester($command);
 
-        $this->assertEquals(Command::SUCCESS, $exitCode);
+        $commandTester->execute([]);
+
+        $this->assertEquals(Command::SUCCESS, $commandTester->getStatusCode());
+        $this->assertStringContainsString(
+            '1 list(s) created',
+            $commandTester->getDisplay()
+        );
+        $this->assertStringContainsString(
+            '0 list(s) renamed',
+            $commandTester->getDisplay()
+        );
+    }
+
+    public function testListRenamed(): void
+    {
+        $application = new Application();
+        $eventBus = Mockery::mock(EventBus::class);
+        $eventBus
+            ->shouldReceive('getPublishedEvents')
+            ->once()
+            ->with()
+            ->andReturn([
+                new ContactListRenamedDomainEvent(
+                    ContactListIdMother::random()->getValue()->getValue(),
+                    ContactListNameMother::random()->getValue()
+                ),
+            ]);
+        $application->add(
+            new ImportContactListsConsoleCommand(
+                new SymfonyMessengerCommandBus(
+                    self::getContainer()->get('command.bus')
+                ),
+                $eventBus
+            )
+        );
+        $command = $application->find(
+            ImportContactListsConsoleCommand::getDefaultName()
+        );
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([]);
+
+        $this->assertEquals(Command::SUCCESS, $commandTester->getStatusCode());
+        $this->assertStringContainsString(
+            '0 list(s) created',
+            $commandTester->getDisplay()
+        );
+        $this->assertStringContainsString(
+            '1 list(s) renamed',
+            $commandTester->getDisplay()
+        );
     }
 }
